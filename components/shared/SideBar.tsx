7e2937d7ad9c436a215b84bad88687b138853788
Savindu Gunasekara <savindugunasekara@gmail.com>
2024-09-14 22:40:00 +0530

    first commit

---
+++ components/shared/SideBar.tsx
@@ -0,0 +1,47 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet";
+
+import NewDocumentButton from "./NewDocumentButton";
+import { MenuIcon } from "lucide-react";
+
+const SideBar = () => {
+
+    const menuOptions =(
+        <>
+
+        <NewDocumentButton/>
+        
+        </>
+    )
+
+  return (
+    <div className="p-2 md:p-5 bg-gray-200 relative">
+
+        <div className="md:hidden">
+        <Sheet>
+        <SheetTrigger>
+          <MenuIcon className="p-2 hover:opacity-30 rounded-lg" size={40} />
+        </SheetTrigger>
+        <SheetContent side={"left"}>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+
+            <div>{menuOptions}</div>
+          </SheetHeader>
+        </SheetContent>
+      </Sheet>
+
+        </div>
+      
+      <div className="hidden md:inline">
+       {menuOptions}
+      </div>
+    </div>
+  );
+};
+export default SideBar;
